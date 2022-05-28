89e635f58684cdd704f318dfac442f7490d9e610
Harry.dev <chihieunguyen9a2@gmail.com>
2022-05-28 10:31:05 +0700

    init commit

---
+++ RxAndroidCallAPI/app/src/main/java/com/hisu/rx_android_call_api/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hisu.rx_android_call_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
